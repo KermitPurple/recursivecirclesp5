86d7bf44bd261958374318612560d9f1e572a359
Shane McDonough <shanemcdman@gmail.com>
2020-03-18 01:11:14 -0700

    Create drawRecursiveCircles function

--- sketch.js
+++ sketch.js
@@ -9,6 +9,16 @@ function draw(){
 	background(0);
 	translate(width/2, height/2)
 	stroke(255);
-	strokeWeight(width/2);
-	point(0,0);
+	noFill();
+	drawRecursiveCircles(0, 0, width/2);
+}
+
+function drawRecursiveCircles(x, y, d){
+	if(d > 2){
+		ellipse(x, y, d);
+		drawRecursiveCircles(x + d/2, y, d/2);
+		drawRecursiveCircles(x - d/2, y, d/2);
+		//drawRecursiveCircles(x, y + d/2, d/2);
+		//drawRecursiveCircles(x, y - d/2, d/2);
+	}
 }
